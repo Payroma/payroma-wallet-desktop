6d16dc8251d574c2cfbeda0e67cae09b682ac530
Payroma Developer <payromafinance@gmail.com>
2022-04-05 17:05:29 +0200

    Wallet details feature completed

--- pmodel/walletdetails.py
+++ pmodel/walletdetails.py
@@ -1,6 +1,6 @@
 from plibs import *
 from pheader import *
-from pcontroller import event
+from pcontroller import payromasdk, event
 from pui import walletdetails
 
 
@@ -12,17 +12,17 @@ class WalletDetailsModel(walletdetails.UiForm, event.EventForm):
         self.events_listening()
 
         # Variables
-        self.__address = None
+        self.__engine = None
 
     def hideEvent(self, a0: QHideEvent):
         super(WalletDetailsModel, self).hideEvent(a0)
         self.reset()
-        self.set_data(self.__address, time.ctime())
+        self.set_data(self.__engine.address().value(), self.__engine.date_created())
 
-    def wallet_changed_event(self, username: str, address: str):
+    def wallet_changed_event(self, engine: payromasdk.engine.wallet.WalletEngine):
         self.reset()
-        self.set_data(address, time.ctime())
-        self.__address = address
+        self.set_data(engine.address().value(), engine.date_created())
+        self.__engine = engine
 
     @pyqtSlot()
     def back_clicked(self):
@@ -30,5 +30,12 @@ class WalletDetailsModel(walletdetails.UiForm, event.EventForm):
 
     @pyqtSlot()
     def private_key_clicked(self):
+        if self.__engine.is_logged():
+            event.authenticatorForward.notify(method=self.__authenticator_forward)
+        else:
+            event.loginForward.notify(method=self.__authenticator_forward)
+
+    def __authenticator_forward(self, private_key: str):
         super(WalletDetailsModel, self).private_key_clicked()
-        self.set_private_key("0" * 66)
+        self.set_private_key(private_key)
+        event.mainTabChanged.notify(tab=Tab.WALLET)
