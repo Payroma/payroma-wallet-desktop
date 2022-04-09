54ed8f888fc2ce536e3f3bd4755a560270f59b5d
Payroma Developer <payromafinance@gmail.com>
2022-04-09 17:59:37 +0200

    Wallet explorer, remove and loggout features are completed

--- pmodel/wallet.py
+++ pmodel/wallet.py
@@ -1,7 +1,7 @@
 from plibs import *
 from pheader import *
-from pcontroller import payromasdk, event
-from pui import wallet
+from pcontroller import payromasdk, event, translator, ThreadingResult, ThreadingArea
+from pui import wallet, styles, images
 from pmodel.tokenslist import TokensListModel
 from pmodel.walletdetails import WalletDetailsModel
 from pmodel.addtoken import AddTokenModel
@@ -18,6 +18,10 @@ class WalletModel(wallet.UiForm, event.EventForm):
         self.add_tab(TokensListModel(self), Tab.WalletTab.TOKENS_LIST)
         self.add_tab(WalletDetailsModel(self), Tab.WalletTab.WALLET_DETAILS)
         self.add_tab(AddTokenModel(self), Tab.WalletTab.ADD_TOKEN)
+
+        # Threading Methods
+        self.__removeThread = ThreadingArea(self.__remove_clicked_core)
+        self.__removeThread.signal.resultSignal.connect(self.__remove_clicked_ui)
 
         # Variables
         self.__engine = None
@@ -59,3 +63,60 @@ class WalletModel(wallet.UiForm, event.EventForm):
     def add_token_clicked(self):
         super(WalletModel, self).add_token_clicked()
         event.walletTabChanged.notify(tab=Tab.WalletTab.ADD_TOKEN)
+
+    def explorer_clicked(self):
+        super(WalletModel, self).explorer_clicked()
+        self.__engine.address().explorer_view()
+
+    def remove_clicked(self):
+        super(WalletModel, self).remove_clicked()
+        if self.__removeThread.isRunning():
+            return
+
+        message1 = translator("This wallet will be removed from the app.")
+        message2 = translator(
+            "Please make sure your wallet's username, password and PIN code are saved before continuing."
+        )
+        message3 = translator("You can import it again by adding a new wallet or importing the backup file.")
+        messagebox = SPGraphics.MessageBoxConfirm(
+            parent=self,
+            text='%s\n\n%s\n\n%s' % (message1, message2, message3),
+            icon=images.data.icons.warning41,
+            color=styles.data.colors.font.name(),
+            accept="Remove",
+            window_size=QSize(401, 231)
+        )
+        messagebox.frame.layout().setContentsMargins(21, 11, 21, 11)
+        messagebox.frame.layout().setSpacing(11)
+        messagebox.exec_()
+        if messagebox.clickedOn is SPGraphics.Button.ACCEPT:
+            self.__removeThread.start()
+
+    def __remove_clicked_core(self):
+        result = ThreadingResult(
+            message=translator("Failed to remove wallet, Please try again")
+        )
+
+        try:
+            result.isValid = payromasdk.engine.wallet.remove(wallet_interface=self.__engine.interface)
+            if result.isValid:
+                result.message = translator("Wallet removed successfully")
+
+        except Exception as err:
+            result.error(str(err))
+
+        self.__removeThread.signal.resultSignal.emit(result)
+
+    @staticmethod
+    def __remove_clicked_ui(result: ThreadingResult):
+        if result.isValid:
+            event.walletEdited.notify()
+            event.mainTabChanged.notify(tab=Tab.WALLETS_LIST)
+
+        result.show_message()
+
+    def logout_clicked(self):
+        super(WalletModel, self).logout_clicked()
+        self.__engine.logout()
+        event.walletEdited.notify()
+        event.mainTabChanged.notify(tab=Tab.WALLETS_LIST)
